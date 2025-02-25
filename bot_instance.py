2a94053d35b5e797dc1e7ef5d7d0b9c6bdc83027
doubleangels <matt@doubleangels.com>
2025-02-24 17:56:56 -0700

    Put main.py all back together

--- bot_instance.py
+++
@@ -1,6 +0,0 @@
-import interactions
-from config import DISCORD_BOT_TOKEN
-
-bot = interactions.Client(
-    intents=(interactions.Intents.DEFAULT | interactions.Intents.MESSAGE_CONTENT | interactions.Intents.GUILD_MEMBERS)
-)
--- utils.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import logging
-from logging_setup import logger
-
-async def safe_task(task):
-    try:
-        await task
-    except Exception as e:
-        logger.exception(f"Exception in safe_task: {e}")
