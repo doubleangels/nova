9313fe30dc32f9b1761175bfd9e6fe2055b67155
doubleangels <matt@doubleangels.com>
2025-02-26 22:27:45 -0700

    Improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,25 @@
 // eslint.config.js
+const recommended = require("eslint/conf/eslint-recommended");
+
 module.exports = [
+  // Include the recommended ESLint config
+  recommended,
   {
-    // Define the files that this configuration applies to.
     files: ["**/*.js"],
-    
-    // Replace the "env" key with languageOptions.
     languageOptions: {
       parserOptions: {
-        // Use ECMAScript 2021 as specified.
         ecmaVersion: 2021,
         sourceType: "module",
       },
-      // Define global variables that are available in both browser and Node environments.
       globals: {
-        // Node globals
         module: "readonly",
         process: "readonly",
         __dirname: "readonly",
         require: "readonly",
-        // Browser globals
         window: "readonly",
         document: "readonly",
       },
     },
-    
-    // Extend the recommended ESLint rules.
-    extends: ["eslint:recommended"],
-    
-    // Define your custom rules.
     rules: {
       semi: ["error", "always"],
     },
