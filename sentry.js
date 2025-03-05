90c2931af55a790efa65fa726aec4b3e38a64afc
Matt VerLee <mverlee7@gmail.com>
2025-03-05 05:37:54 -0700

    Fix error on bot start

--- sentry.js
+++ sentry.js
@@ -1,7 +1,7 @@
 const Sentry = require("@sentry/node");
 const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 const path = require('path');
-const logger = require('../logger')(path.basename(__filename));
+const logger = require('./logger')(path.basename(__filename));
 
 // Initialize Sentry for error tracking and performance monitoring.
 Sentry.init({
