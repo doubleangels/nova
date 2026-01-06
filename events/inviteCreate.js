1cf4f3c73592fa1a9823ae88569188027579401a
doubleangels <matt@doubleangels.com>
2026-01-06 16:19:49 -0700

    Improve discord tracking

---
+++ events/inviteCreate.js
@@ -0,0 +1,58 @@
+const { Events } = require('discord.js');
+const path = require('path');
+const logger = require('../logger')(path.basename(__filename));
+const { getInviteUsage, setInviteUsage } = require('../utils/database');
+
+module.exports = {
+  name: Events.InviteCreate,
+
+  /**
+   * Handles the event when a new invite is created in the guild.
+   * This function updates the invite usage tracking to include the new invite,
+   * ensuring that untagged invites can be tracked when members join.
+   * 
+   * @param {Invite} invite - The invite that was created
+   * @returns {Promise<void>}
+   */
+  async execute(invite) {
+    try {
+      logger.debug('New invite created in guild.', {
+        inviteCode: invite.code,
+        guildId: invite.guild.id,
+        inviterId: invite.inviter?.id,
+        maxUses: invite.maxUses,
+        maxAge: invite.maxAge
+      });
+
+      // Check if bot has permission to view invites
+      const botMember = invite.guild.members.me;
+      if (!botMember?.permissions.has('ManageGuild')) {
+        logger.debug('Bot does not have ManageGuild permission, skipping invite tracking update.');
+        return;
+      }
+
+      // Get current invite usage tracking
+      const currentUsage = await getInviteUsage(invite.guild.id);
+      
+      // Add the new invite to tracking with its current usage count (0 for new invites)
+      currentUsage[invite.code] = invite.uses || 0;
+      
+      // Update the invite usage tracking
+      await setInviteUsage(invite.guild.id, currentUsage);
+      
+      logger.debug('Updated invite usage tracking with new invite.', {
+        inviteCode: invite.code,
+        guildId: invite.guild.id,
+        uses: invite.uses || 0
+      });
+    } catch (error) {
+      logger.error('Error occurred while tracking new invite.', {
+        err: error,
+        inviteCode: invite.code,
+        guildId: invite.guild.id
+      });
+      // Don't throw - this is a non-critical feature
+    }
+  }
+};
+
---
+++ events/inviteDelete.js
@@ -0,0 +1,60 @@
+const { Events } = require('discord.js');
+const path = require('path');
+const logger = require('../logger')(path.basename(__filename));
+const { getInviteUsage, setInviteUsage } = require('../utils/database');
+
+module.exports = {
+  name: Events.InviteDelete,
+
+  /**
+   * Handles the event when an invite is deleted from the guild.
+   * This function removes the invite from usage tracking to keep the database clean.
+   * 
+   * @param {Invite} invite - The invite that was deleted
+   * @returns {Promise<void>}
+   */
+  async execute(invite) {
+    try {
+      logger.debug('Invite deleted from guild.', {
+        inviteCode: invite.code,
+        guildId: invite.guild.id
+      });
+
+      // Check if bot has permission to view invites
+      const botMember = invite.guild.members.me;
+      if (!botMember?.permissions.has('ManageGuild')) {
+        logger.debug('Bot does not have ManageGuild permission, skipping invite tracking cleanup.');
+        return;
+      }
+
+      // Get current invite usage tracking
+      const currentUsage = await getInviteUsage(invite.guild.id);
+      
+      // Remove the deleted invite from tracking
+      if (currentUsage[invite.code] !== undefined) {
+        delete currentUsage[invite.code];
+        
+        // Update the invite usage tracking
+        await setInviteUsage(invite.guild.id, currentUsage);
+        
+        logger.debug('Removed deleted invite from usage tracking.', {
+          inviteCode: invite.code,
+          guildId: invite.guild.id
+        });
+      } else {
+        logger.debug('Deleted invite was not in usage tracking (may have been created before bot started).', {
+          inviteCode: invite.code,
+          guildId: invite.guild.id
+        });
+      }
+    } catch (error) {
+      logger.error('Error occurred while cleaning up deleted invite from tracking.', {
+        err: error,
+        inviteCode: invite.code,
+        guildId: invite.guild.id
+      });
+      // Don't throw - this is a non-critical feature
+    }
+  }
+};
+
